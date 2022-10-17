d856bf7c8d202baa9fbaa71ef8a11e4b856cdddf
Hennc94 <h3nn3ssyc@yahoo.com>
2022-10-17 18:18:29 -0400

    completed assignment

---
+++ 0-data-types/assignments/1-VariablesHw.js
@@ -0,0 +1,35 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+
+const animalName = 'Tiger'
+const height = '3 feet'
+const age = '6 years'
+const shade = 'orange'
+const weight = '250 pounds'
+const length = '10 feet'
+
+const animal = `${animalName} ${height}
+${age} ${shade} ${weight} 
+${length}`
+console.log (animal);
+
+
+const make = 'Dodge'
+const model = 'challenger'
+const color = 'red'
+const horsePower = '700 hp'
+const heavy = '4,000 pounds'
+const cost = '70,000 USD'
+
+const vehicle = `${make} ${model} ${color}
+${horsePower} ${heavy}
+${cost}`
+console.log (vehicle);
