33878b378c349eae4758bd47feb7efaaf0b1227b
Hennc94 <h3nn3ssyc@yahoo.com>
2022-10-22 16:08:58 -0400

    Numbers Homework

---
+++ 3-strings/practice/Test.js
@@ -0,0 +1,25 @@
+let name = "Jack Sparrow";
+let myAge = 30;
+let height = "5'10";
+let food = "Caribbean cuisine";
+const savedTemplateLiteral = `Hello everyone. My name is ${name}. I am approximately ${height}, and ${myAge} years of age. My favorite thing to eat is ${food}`;
+console.log(savedTemplateLiteral);
+// What do you think the above console.log going to produce?
+
+
+/* 
+Copy the following text to your notes, then create variables and assign them strings that will help the bottom console.log run properly:
+Write your code below the comment and above the console log 
+*/
+
+let language= 'javascript';
+let adjective1= 'love';
+let keyword1= 'var';
+let year= '2015';
+let instructor= 'Devon';
+console.log(`I am currently learning the programming language called ${language}. I ${adjective1} this program already. I read recently that declaring variables with the ${keyword1} keyword has gone out of style ever since ECMAScript ${year}. Oh, also, my instructor ${instructor} is awesome!`);
+
+
+// Using only the alphabet variable below, bracket notation & concatenation, console.log out your first name 
+const alph = "abcdefghijklmnopqrstuvwxyz"; 
+console.log(alph[2].toUpperCase() + alph[14] + alph[13] + alph[13] + alph[14] + alph[17]);
