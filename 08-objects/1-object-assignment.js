1eb89ff52f3075b4105490986e6101231ec8ee59
Hennc94 <h3nn3ssyc@yahoo.com>
2022-11-04 22:01:49 -0400

    object homework

---
+++ 08-objects/1-object-assignment.js
@@ -0,0 +1,135 @@
+// 1.   Create an object for your dream vehicle
+//      Give it a minimum of 6 key-value pairs
+//      This object should contain many datatypes (string, number, boolean, null, array)
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+
+// 2.   Using dot-notation Log to the console 3 properties from the previous object
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car.spoiler = 'no spoiler'
+// car.year = '1969'
+// car.make = 'chevrolet'
+
+
+// 3.   Using bracket-notation Log to the console 3 properties from the previous object
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car['spoiler'] = 'no spoiler'
+// car['year'] = '1969'
+// car['make'] = 'chevrolet'
+
+
+
+// 4.   Try and log a property that doesn't already exist - what output do we get?
+//const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car['spoiler'] = 'no spoiler'
+// car['year'] = '1969'
+// car['make'] = 'chevrolet'
+// console.log(car['gear'])
+//value was undefined 
+
+// 5.   Add a new key-value pair to the vehicle. 
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car['spoiler'] = 'no spoiler'
+// car['year'] = '1969'
+// car['make'] = 'chevrolet'
+// car.gear = 'manual'
+// console.log(car['gear'])
+
+// 6.   Using bracket-notation update a property on the vehicle. 
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car['spoiler'] = 'no spoiler'
+// car['year'] = '1969'
+// car['make'] = 'chevrolet'
+// car.gear = 'manual'
+// console.log(car['gear'])
+
+// 7.   Using dot-notation update a property on the vehicle. 
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport'
+// }
+// car['spoiler'] = 'no spoiler'
+// car['year'] = '1969'
+// car['make'] = 'chevrolet'
+// car.gear = 'manual'
+// car.color = 'red'
+// console.log(car)
+// 8.   Create a method for turning your vehicle on
+// const car ={
+//   carType: 'Chevelle',
+//   color: 'Blue',
+//   engine: 'V8',
+//   tires: '24 inch',
+//   model: 'sport',
+  
+//  keyToStart: function(){
+//    console.log ("The car is on!")
+//     }
+
+// }
+// car['keyToStart']
+
+// 9.   Create a method for turning your vehicle off
+const car ={
+    carType: 'Chevelle',
+    color: 'Blue',
+    engine: 'V8',
+    tires: '24 inch',
+    model: 'sport',
+    
+   turnKey: function(){
+     console.log ("The car is off!")
+      }
+  
+  }
+  car['turnKey']
+  
+
+//10.   
+//      a. Check if your vehicle is on (it should be off)
+//      b. Start your vehicle
+//      c. Check if your vehicle is on (it should be on)
+//      d. Stop your vehicle
+//      e. Check if your vehicle is on (it should be on)
+
+
+
+
