bc501e8ae3d667e97b9bd8b59256fd66c5f9ce52
Hennc94 <h3nn3ssyc@yahoo.com>
2023-01-30 19:04:37 -0500

    React HW

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import useStateInCustomProperties from "https://cdn.skypack.dev/use-state-in-custom-properties";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [activeColor, setActiveColor] = useState("red");
+  const CustomPropertiesWrapper = useStateInCustomProperties("box", {
+    activeColor
+  });
+  return (
+    <CustomPropertiesWrapper>
+      <h1 class="is-size-4">Active Color: {activeColor}</h1>
+      <button onClick={() => setActiveColor("red")}>Change to red</button>{" "}
+      <button onClick={() => setActiveColor("blue")}>Change to blue</button>
+    </CustomPropertiesWrapper>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
